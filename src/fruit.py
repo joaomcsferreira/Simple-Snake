0cf8a2c847afde3fb35fe1a583a9396f56310013
João Marcos <56234707+Joao-MCF@users.noreply.github.com>
2020-02-07 11:38:04 -0300

    Update fruit.py

--- src/fruit.py
+++ src/fruit.py
@@ -6,10 +6,11 @@ class Fruit:
     def __init__(self, screen):
         """ a simple idea of a fruit """
         self.position = (randint(0, 39) * 20, randint(0, 29) * 20)
+        self.color = (255, 0, 0)
 
         self.screen = screen
         self.surface = pygame.Surface((15, 15))
-        self.surface.fill((255, 0, 0))
+        self.surface.fill(self.color)
 
     def update(self):
         """ Updates the coordinates of the fruit """
